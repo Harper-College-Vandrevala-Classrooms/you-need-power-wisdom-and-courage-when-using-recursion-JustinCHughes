7f02db091e774def4567eb6161b8414e9ac7a7a8
JustinCHughes <hcjustin713@gmail.com>
2024-10-19 11:02:28 -0500

    Initial solution using loops completed before converting to recursion

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -2,4 +2,32 @@ package com.csc;
 
 public class TriangleNumberCalculator {
   // Your code goes here!
+  public int value(int value)
+  {
+    int x = 1;
+    int answer = 0;
+
+    if(value == 0 || value == 1)
+    {
+      return value;
+    }
+
+    while(x <= value)
+    {
+      answer = x + answer;
+      x++;
+    }
+
+    return answer;
+  }
+
+  public int add(int value1, int value2)
+  {
+    return value(value1) + value(value2);
+  }
+
+  public int subtract(int value1, int value2)
+  {
+    return value(value1) - value(value2);
+  }
 }
--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,6 +1,8 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
 
@@ -11,5 +13,48 @@ public class TestTriangleNumberCalculator {
     calculator = new TriangleNumberCalculator();
   }
 
-  // Your tests go here
+  @Test
+  void oneValueTest() {
+    assertEquals(1,calculator.value(1));
+  }
+
+  @Test
+  void twoValueTest() {
+    assertEquals(3,calculator.value(2));
+  }
+
+  @Test
+  void fourValueTest() {
+    assertEquals(10,calculator.value(4));
+  }
+
+  @Test
+  void oneOneAddTest() {
+    assertEquals(2, calculator.add(1,1));
+  }
+
+  @Test
+  void twoThreeAddTest() {
+    assertEquals(9, calculator.add(2,3));
+  }
+
+  @Test
+  void fourTwoAddTest() {
+    assertEquals(13, calculator.add(4,2));
+  }
+
+  @Test
+  void oneOneSubtractTest() {
+    assertEquals(0, calculator.subtract(1,1));
+  }
+
+  @Test
+  void twoThreeSubtractTest() {
+    assertEquals(-3, calculator.subtract(2,3));
+  }
+
+  @Test
+  void fourTwoSubtractTest() {
+    assertEquals(7, calculator.subtract(4,2));
+  }
 }
